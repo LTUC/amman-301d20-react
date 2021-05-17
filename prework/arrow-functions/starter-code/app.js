31342e0f0cd7a7f6637d7cf3182ffa9add966022
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-05-17 22:27:34 +0300

    answerQuestions

--- prework/arrow-functions/starter-code/app.js
+++ prework/arrow-functions/starter-code/app.js
@@ -257,11 +257,11 @@ Student.prototype.scopeArrow = () => console.log(this);
 // TODO: Uncomment the following line of code to see the output in the browser console
  console.log(joe.scopeArrow());
 
-// TODO: Write a COMMENT below to answer the following questions.
+
+
 // 1. What is "this" when joe.scope() is invoked?
-// undefined output 
-//
+//   the object
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//   undefined output.
+// all the variable passing  to a function
 // 3. Explain why "this" is different when an arrow function is used.
-//The arrow functions   with "this" value   should be in same  enclosling scope 
+//because Arrow functions with "this" based on the scope the Arrow function is defined in.
